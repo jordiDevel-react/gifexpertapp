703424a97bc3773d150b30b1aa96f475ee92125d
jordiDevel <jordi.medinac@gmail.com>
2020-12-02 18:50:12 +0100

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,22 @@
+const getGifs = async (category) => {
+  const giphyKey = 'rVqyoTpTg7QyFqWpneENlXIYy5oUJlVN';
+  const limit = 10;
+  const filter = encodeURI(category);
+
+  const url = `https://api.giphy.com/v1/gifs/search?q=${filter}&limit=${limit}&api_key=${giphyKey}`;
+
+  const response = await fetch(url);
+  const { data } = await response.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
+
+export default getGifs;
